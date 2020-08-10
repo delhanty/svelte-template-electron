357796ae3721ed4b96a085f26238416577337dc0
Paul Delhanty <paul.delhanty_git@cadcambridge.net>
2020-08-10 21:49:56 +0900

    Install "rollup-plugin-terser" for minify when in a production build. (Needs more work.)

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
+import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 
@@ -19,6 +20,10 @@ export default {
 	plugins: [
 		resolve(),
 		svelte({
+			// enable run-time checks when not in production
+			dev: !production,
+			// we'll extract any component CSS out into
+			// a separate file - better for performance
 			css(css) {
 				css.write('dist/svelte.css');
 			},
@@ -27,6 +32,10 @@ export default {
 		commonjs(),
 		json(),
 		typescript({ sourceMap: !production }),
+
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser()
 	],
 
 	external: [
