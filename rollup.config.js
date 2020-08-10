09d661fc962427750a89a7306652afa582de4a4f
Paul Delhanty <paul.delhanty_git@cadcambridge.net>
2020-08-10 15:23:21 +0900

    npm package updates (1) rollup & plugins (2) svelte and tsconfig

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import svelte from 'rollup-plugin-svelte';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import json from 'rollup-plugin-json';
-import typescript from 'rollup-plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
 	input: ['src/main.ts', 'src/renderer.ts'],
