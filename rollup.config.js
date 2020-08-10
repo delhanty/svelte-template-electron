839a021028a743c0c6f88f7b3a9f031190be194d
Paul Delhanty <paul.delhanty_git@cadcambridge.net>
2020-08-10 21:57:01 +0900

    Add `serve()` script to automatically call `npm run dev` as per svelte-typescript-app.

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,27 @@ import typescript from '@rollup/plugin-typescript';
 
 const production = !process.env.ROLLUP_WATCH;
 
+function serve() {
+	let server;
+	
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+				stdio: ['ignore', 'inherit', 'inherit'],
+				shell: true
+			});
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		}
+	};
+}
+
 export default {
 	input: ['src/main.ts', 'src/renderer.ts'],
 
@@ -32,6 +53,10 @@ export default {
 		commonjs(),
 		json(),
 		typescript({ sourceMap: !production }),
+
+		// In dev mode, call `npm run start` once
+		// the bundle has been generated
+		!production && serve(),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
